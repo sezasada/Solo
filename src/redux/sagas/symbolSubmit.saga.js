b814654ad6d33f7375060411d71b9f64b4e7f092
Sebastien Zasada <sezasada@icloud.com>
2023-04-12 11:53:45 -0500

    now a user can input a company name/ticker to see earnings reports

--- src/redux/sagas/symbolSubmit.saga.js
+++ src/redux/sagas/symbolSubmit.saga.js
@@ -1,14 +1,41 @@
-import { put, takeEvery } from 'redux-saga/effects';
+import { put, takeEvery } from "redux-saga/effects";
+
+import axios from "axios";
 
 function* handleSubmit(action) {
-    const symbolInput = action.payload;
-    yield put({ type: "SET_SELECTED_SYMBOL", payload: symbolInput });
-    yield put({ type: "FETCH_EARNINGS" });
+  const searchTerm = action.payload;
+  let symbol = null;
+
+  try {
+    // Fetch company details using the search term
+    const response = yield axios.get(
+      `https://financialmodelingprep.com/api/v3/search?query=${searchTerm}&limit=10&exchange=NASDAQ&apikey=19198710f19b50ecd5513c63a590ad31`
+    );
+
+    // Find the first matching company with the same symbol or name as the search term
+    const foundCompany = response.data.find(
+      (company) =>
+        company.symbol === searchTerm.toUpperCase() ||
+        company.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    if (foundCompany) {
+      symbol = foundCompany.symbol;
+    } else {
+      throw new Error("No results found");
+    }
+  } catch (error) {
+    console.log("Error fetching company details:", error);
+    yield put({ type: "FETCH_EARNINGS_ERROR", payload: error });
+    return;
+  }
+
+  yield put({ type: "SET_SELECTED_SYMBOL", payload: symbol });
+  yield put({ type: "FETCH_EARNINGS" });
 }
 
-// watcher saga to trigger the handleSubmit saga when the SUBMIT_SYMBOL action is dispatched
 function* watchSubmitSymbol() {
-    yield takeEvery('SUBMIT_SYMBOL', handleSubmit);
+  yield takeEvery("SUBMIT_SYMBOL", handleSubmit);
 }
 
 export default watchSubmitSymbol;
