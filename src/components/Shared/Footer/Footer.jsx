2aeb36bc7409b9fa0d91c76aaee47d9d1de38ee0
Sebastien Zasada <sezasada@icloud.com>
2023-04-12 20:45:22 -0500

    had to change react versions, also deleted the footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -7,11 +7,11 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return 
+  return (
     <div className='foot'>
-      <footer>&copy; Sebastien Zasada</footer>;
+      
     </div>
-  
+  );
 }
 
 export default Footer;
