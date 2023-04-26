bfc539e06b02f17e7351cd1c3da0f1760966db2c
Sebastien Zasada <sezasada@icloud.com>
2023-04-26 12:54:26 -0500

    added openai api and styling. everything is running well

--- src/components/Pages/ChatPage/ChatPage.jsx
+++ src/components/Pages/ChatPage/ChatPage.jsx
@@ -5,7 +5,7 @@ import "./ChatPage.css";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import { TextField, FormControl, Button } from "@mui/material";
 import { InputAdornment, IconButton } from "@mui/material";
-
+import SmartToyIcon from "@mui/icons-material/SmartToy";
 function ChatPage() {
   const [inputText, setInputText] = useState("");
   const [chatHistory, setChatHistory] = useState([]);
@@ -13,7 +13,7 @@ function ChatPage() {
   const generatedText = useSelector(
     (store) => store.earningsReducer.generatedText
   );
-  const user = useSelector((store) => store.earningsReducer.user);
+  const user = useSelector((store) => store.user.username);
   const dispatch = useDispatch();
 
   const generateTextRequest = (text) => ({
@@ -45,20 +45,63 @@ function ChatPage() {
             alignItems: "center",
           }}
         >
-          <div style={{ maxWidth: "60%", width: "100%" }}>
+          <div style={{ width: "100%" }}>
             {chatHistory.map((chat, index) => (
               <div key={index}>
-                <div style={{ textAlign: "left" }}>
-                  User: {user}
-                  {chat.userText}
+                <div
+                  style={{
+                    paddingLeft: "20%",
+                    paddingTop: "1vh",
+                    paddingBottom: "1vh",
+                    paddingRight: "20%",
+                    width: "100%",
+                  }}
+                >
+                  <div style={{ textAlign: "left", width: "100%" }}>
+                    <div
+                      style={{
+                        backgroundColor: "grey",
+                        color: "white",
+                        display: "inline-block",
+                        padding: "0.4em",
+                        borderRadius: "5px",
+                        marginRight: "0.5em",
+                      }}
+                    >
+                      {user}
+                    </div>
+
+                    {chat.userText}
+                  </div>
                 </div>
-                <div style={{ textAlign: "left" }}>AI: {chat.aiText}</div>
+                <div
+                  style={{
+                    backgroundColor: "#D3D3D3",
+                    paddingLeft: "20%",
+                    paddingTop: "1.8vh",
+                    paddingBottom: "1.8vh",
+                    paddingRight: "20%",
+                    width: "100%",
+                  }}
+                >
+                  <div style={{ textAlign: "left", width: "100%" }}>
+                    <SmartToyIcon
+                      style={{
+                        fontSize: "30px",
+                        marginRight: "0.5em",
+                      }}
+                    />
+                    {chat.aiText}
+                  </div>
+                </div>
               </div>
             ))}
           </div>
-          <div className="input-containersssss">
+          <div className="input-containersssss" style={{ width: "80%" }}>
             <FormControl
               sx={{
+                width: "100%",
+                maxWidth: "800px",
                 "margin-left": "2px",
                 "margin-right": "2px",
                 "padding-left": "2px",
@@ -79,7 +122,7 @@ function ChatPage() {
               <TextField
                 sx={{
                   marginBottom: 1,
-                  width: 300,
+
                   "margin-left": "2px",
                   "margin-right": "2px",
                   "padding-left": "2px",
@@ -97,6 +140,7 @@ function ChatPage() {
                   },
                 }}
                 id="outlined-basic"
+                placeholder="Send a Message."
                 variant="outlined"
                 value={inputText}
                 onChange={(e) => setInputText(e.target.value)}
