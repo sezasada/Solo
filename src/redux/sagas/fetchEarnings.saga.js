f777c43763021fef3ddd77c8f1f6362ff15d3a1b
Sebastien Zasada <sezasada@icloud.com>
2023-04-27 13:21:57 -0500

    lowered the amount of API calls

--- src/redux/sagas/fetchEarnings.saga.js
+++ src/redux/sagas/fetchEarnings.saga.js
@@ -1,18 +1,18 @@
 import { put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
 
 function* fetchEarnings() {
-    try {
-        const response = yield fetch('/api/earnings/earnings');
-        const data = yield response.json();
-        yield put({ type: 'SET_EARNINGS', payload: data });
-    } catch (error) {
-        console.log('Error fetching earnings', error);
-    }
+  try {
+    const response = yield axios.get('/api/earnings/earnings');
+    const data = response.data;
+    yield put({ type: 'SET_EARNINGS', payload: data });
+  } catch (error) {
+    console.log('Error fetching earnings', error);
+  }
 }
 
-
 function* watchFetchEarnings() {
-    yield takeEvery('FETCH_EARNINGS', fetchEarnings);
+  yield takeEvery('FETCH_EARNINGS', fetchEarnings);
 }
 
 export default watchFetchEarnings;
